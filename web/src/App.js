1d236ad04bf146f7a008dbb824a3402663afa40a
kerwinreyes <kerwinfelixreyes@gmail.com>
2024-06-05 00:11:06 +0800

    [main] add instructions

--- web/src/App.js
+++ web/src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Ela from './pages/ela';
 import CustomizeName from './pages/others';
+import Instruction from './pages/instructions';
 const App = () => {
 
     return (
       <BrowserRouter>
         <Routes>
-          <Route path="/" />
+          <Route path="/" element={<Instruction />} />
           <Route path="/ela" element={<Ela />} />
           <Route path="/:type/:name" element={<CustomizeName />} />
         </Routes>
