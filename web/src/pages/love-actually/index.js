14adae2d8ad8f2341ffec8293338b11eb9c8e5c9
kerwin0831 <kerwin083@gmail.com>
2024-12-24 20:09:00 +0800

    fix build

--- web/src/pages/love-actually/index.js
+++ web/src/pages/love-actually/index.js
@@ -3,13 +3,6 @@ import React, { useRef } from 'react';
 const LoveActually = () => {
   const audioRef = useRef(null);
 
-  const handlePlay = () => {
-    // Play the audio when the button is clicked
-    if (audioRef.current) {
-      audioRef.current.play();
-    }
-  };
-
   const cards = [
     {
       div: <div> Hi you <br/> <span style={{ color: 'gray', fontSize: "12px" }}> (swipe left)</span>  </div>
@@ -25,6 +18,7 @@ const LoveActually = () => {
           height:"300px",
           objectFit:"contain"
         }}
+        alt="love-actually-1"
         src="https://static1.thegamerimages.com/wordpress/wp-content/uploads/2021/02/pjimage-2021-02-19T174626.661-1.jpg" />
         </div>
     },
@@ -39,7 +33,8 @@ const LoveActually = () => {
           height: "auto",
           objectFit: "contain"
         }}
-      src="https://scontent-sin2-1.xx.fbcdn.net/v/t39.30808-6/460826164_7398836476886534_6482392898617403982_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=0b6b33&_nc_ohc=FVdl1br7hF8Q7kNvgHiqVYK&_nc_zt=23&_nc_ht=scontent-sin2-1.xx&_nc_gid=A0xrTlfq53NL-bQ4dqmxFp4&oh=00_AYCdyH3y4UvZSRlAGEIQWN8gKe7othZEJXe0xAOkXU_sHw&oe=67707455" />
+        alt="love-actually-2"
+        src="https://scontent-sin2-1.xx.fbcdn.net/v/t39.30808-6/460826164_7398836476886534_6482392898617403982_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=0b6b33&_nc_ohc=FVdl1br7hF8Q7kNvgHiqVYK&_nc_zt=23&_nc_ht=scontent-sin2-1.xx&_nc_gid=A0xrTlfq53NL-bQ4dqmxFp4&oh=00_AYCdyH3y4UvZSRlAGEIQWN8gKe7othZEJXe0xAOkXU_sHw&oe=67707455" />
     </div>
   },
   {
@@ -66,6 +61,7 @@ const LoveActually = () => {
         height: "auto",
         objectFit: "contain"
       }}
+      alt="love-actually-3"
       src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/TheElderLady-61070-FrontView-PlateXCVII-TheRoyalMummies-1912.gif/699px-TheElderLady-61070-FrontView-PlateXCVII-TheRoyalMummies-1912.gif" />
     </div>
   },
